c55f199f1e90cae91f3b98be8a922381333ff674
loganchodev <loganchodev@gmail.com>
2024-03-27 14:45:43 +0900

    페이지 라우팅

---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import {useSearchParams } from "react-router-dom";
+
+const Home = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    console.log(searchParams.get("sort"));
+
+    return <div>Home 페이지입니다.</div>
+};
+
+export default Home;
