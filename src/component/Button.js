754a613ea3187585c5b82b2b47c1362f487534fa
loganchodev <loganchodev@gmail.com>
2024-03-29 13:05:43 +0900

    Home.js 수정 버튼 추가

--- src/component/Button.js
+++ src/component/Button.js
@@ -1,11 +1,18 @@
 import "./Button.css";
 
 const Button = ({text, type, onClick }) => {
+    const btnType = ["positive", "negative"].includes(type) ? type : "default";
     return (
-        <button className="Button" onClick={onClick}>
+        <button className={["Button", `Button_${btnType}`].join(" ")}
+                onClick={onClick}
+        >
             {text}
         </button>
     );
+};
+
+Button.defaultProps = {
+    type: "default",
 };
 
 export default Button;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,9 +5,23 @@ const Home = () => {
     return (
         <div>
             <Button
-                text={"버튼 텍스트"}
+                text={"기본 버튼"}
                 onClick={()=>{
-                    alert("hi");
+                    alert("default button");
+                }}
+            />
+            <Button
+                type="positive"
+                text={"긍정 버튼"}
+                onClick={()=>{
+                    alert("positive button");
+                }}
+            />
+            <Button
+                type="negative"
+                text={"부정 버튼"}
+                onClick={()=>{
+                    alert("negative button");
                 }}
             />
         </div>
